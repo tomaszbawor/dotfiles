b9f07135b4436cb71326568bbc00bb93ba78221f
Tomasz Bawor <bawortomasz@gmail.com>
2024-03-29 16:02:55 +0100

    add copilot

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -21,6 +21,8 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.linting.eslint" },
     { import = "lazyvim.plugins.extras.lang.tailwind" },
 
+    { import = "lazyvim.plugins.extras.coding.copilot" },
+
     { import = "lazyvim.plugins.extras.editor.harpoon2" },
     -- import/override with your plugins
     { import = "plugins" },
