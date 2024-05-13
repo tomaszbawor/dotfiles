df43fbabc8c2372809be9f79ca70ecfba793ff9b
Tomasz Bawor <bawortomasz@gmail.com>
2024-05-14 00:21:16 +0200

    Some more configuration

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -57,7 +57,7 @@
   programs.git = {
       enable = true;
       userName = "Tomasz Bawor";
-        userEmail = "bawortomasz@gmail.com";
+      userEmail = "bawortomasz@gmail.com";
       };
       # Home Manager can also manage your environment variables through 'home.sessionVariables'. These will be explicitly sourced when using a shell provided by Home Manager. If you don't want to manage your shell through Home Manager then you have to manually source 'hm-session-vars.sh' located at either ~/.nix-profile/etc/profile.d/hm-session-vars.sh
   # or
