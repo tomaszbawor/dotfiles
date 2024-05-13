a8afde0538ac6604bfd27eb802347e3ae93ab4e5
Tomasz Bawor <bawortomasz@gmail.com>
2024-05-14 00:16:51 +0200

    Some basic configuration

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -18,11 +18,11 @@
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = [
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-  # pkgs.hello
-  pkgs.jq
-  pkgs.lazygit
+    pkgs.jq
+    pkgs.lazygit
+    pkgs.zoxide
+    pkgs.rustup
+    pkgs.docker
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
@@ -37,10 +37,11 @@
     #   echo "Hello, ${config.home.username}!"
     # '')
   ];
-
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
   home.file = {
+    ".zshrc".source = ./../zshrc/zshrc-config;
+    ".config/nvim" = {source = ./../nvim; recursive = true;};
     # # Building this configuration will create a copy of 'dotfiles/screenrc' in
     # # the Nix store. Activating the configuration will then make '~/.screenrc' a
     # # symlink to the Nix store copy.
@@ -52,15 +53,13 @@
     #   org.gradle.daemon.idletimeout=3600000
     # '';
   };
-
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. These will be explicitly sourced when using a
-  # shell provided by Home Manager. If you don't want to manage your shell
-  # through Home Manager then you have to manually source 'hm-session-vars.sh'
-  # located at either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
+# Git Configuration 
+  programs.git = {
+      enable = true;
+      userName = "Tomasz Bawor";
+        userEmail = "bawortomasz@gmail.com";
+      };
+      # Home Manager can also manage your environment variables through 'home.sessionVariables'. These will be explicitly sourced when using a shell provided by Home Manager. If you don't want to manage your shell through Home Manager then you have to manually source 'hm-session-vars.sh' located at either ~/.nix-profile/etc/profile.d/hm-session-vars.sh
   # or
   #
   #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
