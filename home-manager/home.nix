6cb305cc7e036597cf75a2e7a266a325ebf0a9c0
Tomasz Bawor <bawortomasz@gmail.com>
2024-05-15 13:35:05 +0200

    Enable go

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -128,6 +128,8 @@
     enable = true;
     package = pkgs.temurin-bin-21;
   };
+
+  programs.go.enable = true;
   # Home Manager can also manage your environment variables through 'home.sessionVariables'. These will be explicitly sourced when using a shell provided by Home Manager. If you don't want to manage your shell through Home Manager then you have to manually source 'hm-session-vars.sh' located at either ~/.nix-profile/etc/profile.d/hm-session-vars.sh
   # or
   #
