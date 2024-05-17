50abdc1faf25affbd9710de6e17b7d0b74a20dad
Tomasz Bawor <bawortomasz@gmail.com>
2024-05-17 14:15:08 +0200

    clean comments

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -37,9 +37,6 @@
     ];
   };
 
-
-
-
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = [
@@ -50,6 +47,7 @@
     pkgs.ripgrep
     pkgs.bat
     pkgs.eza
+    pkgs.neovim
 
     # git
     pkgs.diff-so-fancy
@@ -68,36 +66,13 @@
     # fonts
     (pkgs.nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
 
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
-
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
   ];
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
   home.file = {
-    # ".zshrc".source = ./../zshrc/zshrc-config;
     ".config/nvim" = { source = ./../nvim; recursive = true; };
     ".config/nix/nix.conf".source = ./nix.conf;
     ".config/zellij" = { source = ./../zellij; recursive = true; };
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
   };
 
   programs.zsh = {
@@ -151,15 +126,9 @@
   };
 
   programs.go.enable = true;
-  # Home Manager can also manage your environment variables through 'home.sessionVariables'. These will be explicitly sourced when using a shell provided by Home Manager. If you don't want to manage your shell through Home Manager then you have to manually source 'hm-session-vars.sh' located at either ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/tomasz/etc/profile.d/hm-session-vars.sh
-  #
+
+  programs.chromium.enable = true;
+
   home.sessionVariables = {
     EDITOR = "nvim";
   };
